c7493f40b8b18c4d668eee27e9e5668431e9eeae
nick-pell <93624627+nick-pell@users.noreply.github.com>
2024-04-17 18:21:19 -0400

    Added custom stock option and some text

--- client/src/components/Stocks.js
+++ client/src/components/Stocks.js
@@ -1,45 +1,72 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const Stocks = ({ onForecastData }) => {
-  const [selectedStock, setSelectedStock] = useState('AAPL'); // Default stock symbol
+  const [selectedStock, setSelectedStock] = useState("AAPL"); // Default stock symbol
+  const [customStock, setCustomStock] = useState(""); // State to hold custom stock input
   const [years, setYears] = useState(2); // Default number of years
+  const [isValidStock, setIsValidStock] = useState(true); // State to track valid stock ticker
 
   const handleForecast = async () => {
     try {
-      const response = await axios.get('http://127.0.0.1:5000/get_stock', {
+      const stockToFetch =
+        selectedStock === "other" ? customStock : selectedStock;
+      const response = await axios.get("http://127.0.0.1:5000/get_stock", {
         params: {
-          stock: selectedStock,
-          years: years
-        }
+          stock: stockToFetch,
+          years: years,
+        },
       });
       onForecastData(response.data);
+      setIsValidStock(true); // Resetting to true when fetching is successful
     } catch (error) {
-      console.error('Error fetching forecast data:', error);
+      console.error("Error fetching forecast data:", error);
+      setIsValidStock(false); // Setting to false when fetching fails
     }
+  };
+
+  const handleCustomStockChange = (e) => {
+    setCustomStock(e.target.value);
+    setIsValidStock(true); // Resetting validity on input change
   };
 
   return (
     <div>
       <label>
         Select stock:
-        <select value={selectedStock} onChange={e => setSelectedStock(e.target.value)}>
+        <select
+          value={selectedStock}
+          onChange={(e) => {
+            setSelectedStock(e.target.value);
+            setIsValidStock(true); // Resetting validity when selecting from dropdown
+          }}
+        >
           <option value="GOOG">GOOG</option>
           <option value="AAPL">AAPL</option>
           <option value="MSFT">MSFT</option>
           <option value="GME">GME</option>
+          <option value="other">Other</option>
         </select>
+        {selectedStock === "other" && (
+          <input
+            type="text"
+            placeholder="Enter Stock Ticker"
+            value={customStock}
+            onChange={handleCustomStockChange}
+          />
+        )}
       </label>
       <label>
         Select years of prediction:
         <input
           type="number"
           value={years}
-          onChange={e => setYears(parseInt(e.target.value))}
+          onChange={(e) => setYears(parseInt(e.target.value))}
           min="1"
           max="5"
         />
       </label>
+      {!isValidStock && <p style={{ color: "red" }}>Invalid stock ticker</p>}
       <button onClick={handleForecast}>Get Forecast</button>
     </div>
   );
--- client/src/pages/StockPage.js
+++ client/src/pages/StockPage.js
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Stocks from "../components/Stocks";
 import StockPlot from "../components/StockPlot";
 
 function StockPage() {
-    const [forecastData, setForecastData] = useState(null);
-    const [error, setError] = useState(null);
-  
-    const handleForecastData = (data) => {
-      setForecastData(data);
-    };  
-  
-    return (
-        <div>
-        <h1>Stock Forecast</h1>
-        <Stocks onForecastData={handleForecastData}/>
-        {error && <p>Error: {error}</p>}
-        {forecastData && <StockPlot data={forecastData} />}    
-        </div>
-    )
-    }
-  
-  export default StockPage;
+  const [forecastData, setForecastData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleForecastData = (data) => {
+    setForecastData(data);
+  };
+
+  return (
+    <div>
+      <h1>Stock Forecast</h1>
+      <p>
+        Investing in stocks is a powerful way to build wealth by tapping into
+        the financial successes of top companies. However, the stock market can
+        be complex and unpredictable. Our advanced stock predictor tool
+        simplifies these challenges by providing you with precise, data-driven
+        insights, enabling smarter investment decisions. Enhance your investment
+        strategy and boost your returns—try our stock predictor today and invest
+        with confidence!
+      </p>
+      <Stocks onForecastData={handleForecastData} />
+      {error && <p>Error: {error}</p>}
+      {forecastData && <StockPlot data={forecastData} />}
+    </div>
+  );
+}
+
+export default StockPage;
