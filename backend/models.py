df94927ee829fd9b337b4d73edc7f93196e26600
nick-pell <93624627+nick-pell@users.noreply.github.com>
2024-04-16 21:51:20 -0400

    Catching up with main
    Catching up with main and putting my additions in

--- backend/models.py
+++ backend/models.py
@@ -1,21 +1,21 @@
 #Contains SQLAlchemy model definitions
 
-from config import db
+# from config import db
 
-class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80), unique=False, nullable=False)
-    last_name = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)    #email will act as username
+# class Contact(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     first_name = db.Column(db.String(80), unique=False, nullable=False)
+#     last_name = db.Column(db.String(80), unique=False, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)    #email will act as username
 
-    password = db.Column(db.String(15), unique=False, nullable=False)
+#     password = db.Column(db.String(15), unique=False, nullable=False)
 
 
-    def to_json(self):
-        return {
-            "id": self.id,
-            "firstName": self.first_name,
-            "lastName": self.last_name,
-            "email": self.email,
-            "password": self.password,
-        }
+#     def to_json(self):
+#         return {
+#             "id": self.id,
+#             "firstName": self.first_name,
+#             "lastName": self.last_name,
+#             "email": self.email,
+#             "password": self.password,
+#         }
