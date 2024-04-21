e7fd1e2152c6ef4fe9a05f41e30201adbf4b1eb0
LeiliZadeh <143642741+LeiliZadeh@users.noreply.github.com>
2024-04-21 00:32:10 -0400

    landing page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,15 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './LandingPage.css';  
 
 function LandingPage() {
   return (
-    <div>
-      <h1>Welcome to Our App!</h1>
-      <p>Learn more about our app here...</p>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
-    </div>
+    <>
+      <div className="content">
+        <div className="text-section">
+          <h1>Money Tree AI Finance</h1>
+          <Link to="/login">
+            <button className="button">Login / Sign up</button>
+          </Link>
+        </div>
+        <div className="image-section">
+          {/* In case of error replace  with your image path */}
+          <img src="/financepage-removebg.png" alt="Financial Analysis" />
+        </div>
+      </div>
+      <div className="description">
+        <p>
+          Money Tree is designed to provide personalized financial advice through
+          AI and machine learning technologies. It analyzes your financial
+          data—such as expenses, income, and savings—to offer tailored
+          recommendations for budgeting, saving, investing, and managing debt.
+          This user-friendly platform guides you through entering your financial
+          details, enabling the AI to deliver actionable insights and customized
+          financial strategies. Prioritizing your security, it ensures all
+          sensitive data is handled with the utmost care, making personal finance
+          management accessible, insightful, and secure.
+        </p>
+      </div>
+    </>
   );
 }
 
